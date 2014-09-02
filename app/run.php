1a694707c52dd3b2de84f4f546d18c3b5820636c
Zizaco <zizaco@gmail.com>
2014-09-02 12:59:51 -0300

    Tweaked identation in run.php

--- app/run.php
+++ app/run.php
@@ -1,4 +1,5 @@
 <?php
-    $console = $app->make('console');
-    $console->add($app->make('Dployer\Command\Deploy'));
-    $console->run();
+
+$console = $app->make('console');
+$console->add($app->make('Dployer\Command\Deploy'));
+$console->run();
