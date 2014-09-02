d57c8fcd18a15c0ec1b004a59c2f9a0186b2dddd
Zizaco <zizaco@gmail.com>
2014-09-02 13:13:27 -0300

    Added EBSVersionManager in order to send and set version of projects in EBS

---
+++ app/Dployer/Services/EBSVersionManager.php
@@ -0,0 +1,135 @@
+<?php
+namespace Dployer\Services;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class EBSVersionManager
+ *
+ * Manages project versions in AWS ElasticBeanstalk.
+ *
+ * @package  Dployer\Services
+ */
+class EBSVersionManager
+{
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * EBS Application name
+     * @var string
+     */
+    protected $app;
+
+    /**
+     * EBS Environment name
+     * @var string
+     */
+    protected $env;
+
+    /**
+     * Base class for interacting with web service clients
+     * @var Aws\Common\Aws
+     */
+    protected $aws;
+
+    /**
+     * Set the app attribute using the global $app variable
+     */
+    public function __construct()
+    {
+        $this->app = app();
+
+        $aws = $this->app->make('Aws\Common\Aws');
+        $this->aws = $aws->factory(getenv("HOME").'/.aws/config.json');
+    }
+
+    /**
+     * Initializes the EBSVersionManager
+     *
+     * @param  string $app     Application name
+     * @param  string $env     Application environment name
+     * @param  OutputInterface $output
+     */
+    public function init($app, $env, OutputInterface $output)
+    {
+        $this->app    = $app;
+        $this->env    = $env;
+        $this->output = $output;
+    }
+
+    /**
+     * Creates a new application version with the given local file
+     *
+     * @param  string $filename    Local zip file to be used as the application version
+     * @param  string $description
+     *
+     * @return string VersionLabel
+     */
+    public function createVersion($filename, $description = "")
+    {
+        if (! getenv('DPLOYER_BUCKET')) {
+            $this->output->writeln("<error>DPLOYER_BUCKET environment variable not set</error>");
+        }
+
+        $bucket       = getenv('DPLOYER_BUCKET') ?: 'dployer-versions';
+        $key          = str_replace('.zip', '-'.date('U').'.zip', $filename);
+        $versionLabel = $key;
+
+        $s3 = $this->aws->get('S3');
+        $s3->registerStreamWrapper();
+
+        $this->output->writeln("Uploading <info>$filename</info>...");
+        copy(
+            getcwd().'/'.$filename,
+            's3://'.$bucket.'/'.$key
+        );
+
+        $ebs = $this->aws->get('ElasticBeanstalk');
+
+        $this->output->writeln("Creating version <info>$versionLabel</info> in EBS...");
+        $ver = $ebs->createApplicationVersion([
+            'ApplicationName' => $this->app,
+            'VersionLabel'    => $versionLabel,
+            'Description'     => $description ?: "No description",
+            'SourceBundle'    => [
+                'S3Bucket' => $bucket,
+                'S3Key'    => $key,
+            ],
+            'AutoCreateApplication' => false,
+        ]);
+
+        if ($ver->get('ApplicationVersion')) {
+            return $versionLabel;
+        }
+
+        return false;
+    }
+
+    /**
+     * Deploy the version of the given $versionLabel of the application
+     *
+     * @param  string $versionLabel Identifier of the version that is going to be used
+     *
+     * @return boolean Success
+     */
+    public function deployVersion($versionLabel)
+    {
+        $ebs = $this->aws->get('ElasticBeanstalk');
+
+        $this->output->writeln("Deploying version <info>$versionLabel</info> to <info>".$this->app." ".$this->env."</info>...");
+        $env = $ebs->updateEnvironment([
+            "ApplicationName" => $this->app,
+            "EnvironmentName" => $this->env,
+            "VersionLabel" => $versionLabel,
+        ]);
+
+        if ($env->get('VersionLabel') == $versionLabel) {
+            return true;
+        }
+
+        return false;
+    }
+}
