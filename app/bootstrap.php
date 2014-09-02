f4c28b374ffed7e6e15d54a870bf071e5c8a96e3
Zizaco <zizaco@gmail.com>
2014-09-02 13:06:15 -0300

    Added app function in order to return the global app object

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,11 +4,17 @@
 $app = new Illuminate\Container\Container();
 
 // Bind isolator for PHP functions
-$app->bind('php', function() {
+$app->bind('php', function () {
     return new Icecave\Isolator\Isolator;
 }, true);
 
 // Bind the Symfony Console application
-$app->bind('console', function() {
+$app->bind('console', function () {
     return new Symfony\Component\Console\Application('Dployment', '0.1 BETA');
 }, true);
+
+function app()
+{
+    global $app;
+    return $app;
+}
